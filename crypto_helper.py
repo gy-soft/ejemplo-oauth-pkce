500d19541229fab96f343d78079337f74f90c48c
Alan Ramos <ramos.acrp@gmail.com>
2021-02-12 22:07:33 -0600

    pkce flow

--- crypto_helper.py
+++ crypto_helper.py
@@ -1,20 +1,28 @@
 from Crypto.Random.random import StrongRandom
+from Crypto.Hash.SHA256 import SHA256Hash
 import base64
 import webbrowser
 
 OAUTH_HOST = "https://www.dropbox.com/oauth2/authorize"
 CLIENT_ID = "tpr1swzr3p44n26"
+BASE_URL = "{0}?client_id={1}&response_type=code&code_challenge_method=S256&code_challenge={2}"
 
-def get_url():
-  "{}?"
-  return OAUTH_HOST + 
+def get_url(challenge):
+  return BASE_URL.format(OAUTH_HOST, CLIENT_ID, challenge)
 
 class CryptoHelper():
   def __init__(self):
     self.rnd = StrongRandom()
 
-  def get_random(self):
+  def get_code(self):
     mapped = map(lambda x: self.rnd.getrandbits(8), range(32))
-    encoded = base64.b64encode(bytes(mapped))
-    webbrowser.open()
-    print(str(encoded))
+    encoded = base64.urlsafe_b64encode(bytes(mapped))
+    verifier = encoded.decode().replace("=", "")
+    hash = SHA256Hash.new(self)
+    hash.update(verifier.encode("utf-8"))
+    digest = base64.urlsafe_b64encode(hash.digest())
+    challenge = digest.decode().replace("=", "")
+    return {
+      "verifier": verifier,
+      "challenge": challenge
+    }
--- oauth_window.py
+++ oauth_window.py
@@ -2,20 +2,65 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+from webbrowser import open as open_browser
+import requests
 from crypto_helper import CryptoHelper
+
+OAUTH_HOST = "https://www.dropbox.com/oauth2/authorize"
+TOKEN_HOST = "https://api.dropbox.com/oauth2/token"
+API_HOST = "https://api.dropboxapi.com/2/files/list_folder"
+CLIENT_ID = "tpr1swzr3p44n26"
+OAUTH_URL = "{0}?client_id={1}&response_type=code&code_challenge_method=S256&code_challenge={2}"
+TOKEN_URL = "{0}?client_id={1}&grant_type=authorization_code&code={2}&code_verifier={3}"
 
 class OauthWindow(Gtk.Window):
   def __init__(self):
     Gtk.Window.__init__(self, title="OAuth authenticator")
 
+    self.code_verifier = ""
+    self.token = "A3VpxU4SttgAAAAAAAAAAUSOSPNhsYpSnQujjJAjXdj_E5L2kV6kWiWQFfU9g0xv"
     self.crypto_helper = CryptoHelper()
 
     builder = Gtk.Builder()
     builder.add_from_file("./main.glade")
     container = builder.get_object("container")
-    button = builder.get_object("btn_auth")
-    button.connect("clicked", self.print_random)
+    btn_getcode = builder.get_object("btn_get_code")
+    btn_getcode.connect("clicked", self.grant_app)
+    btn_auth = builder.get_object("btn_auth")
+    btn_auth.connect("clicked", self.authenticate)
+    btn_test = builder.get_object("btn_test")
+    btn_test.connect("clicked", self.call_api)
+    self.txt_authcode = builder.get_object("txt_auth_code")
+    self.txt_response = builder.get_object("txt_response")
     self.add(container)
 
-  def print_random(self, widget):
-    self.crypto_helper.get_random()
+  def grant_app(self, widget):
+    code = self.crypto_helper.get_code()
+    self.code_verifier = code["verifier"]
+    auth_url = OAUTH_URL.format(OAUTH_HOST, CLIENT_ID, code["challenge"])
+    open_browser(auth_url)
+
+  def authenticate(self, widget):
+    oauth_code = self.txt_authcode.get_text()
+    params = {
+      "client_id": CLIENT_ID,
+      "grant_type": "authorization_code",
+      "code": oauth_code,
+      "code_verifier": self.code_verifier
+    }
+    response = requests.post(TOKEN_HOST, params)
+    json = response.json()
+    self.token = json["access_token"]
+    print(json)
+
+  def call_api(self, widget):
+    json = {
+      "path": ""
+    }
+    headers = {
+      "Authorization": "Bearer {0}".format(self.token)
+    }
+    response = requests.post(API_HOST,headers=headers,json=json)
+    json = response.text
+    buffer = self.txt_response.get_buffer()
+    buffer.set_text(json.replace("{", "{\n").replace("}", "}\n").replace(",", ",\n"))
